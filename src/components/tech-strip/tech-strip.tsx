20880ce40358c7d6111f0cbb3c0f5c0a6638de1b
Adam Flatley <flatleyadam@yahoo.co.uk>
2023-10-09 14:24:53 +0100

    add some content - begin tech strip component

--- src/components/tech-strip/tech-strip.tsx
+++ src/components/tech-strip/tech-strip.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import GithubLogo from '../../../images/GitHub-Emblem.png'
 import GQLLogo from '../../../images/graphql-logo.png'
+import JavaLogo from '../../../images/Java-Logo.png'
+import SpringBootLogo from '../../../images/spring-boot-logo.png'
+import TSLogo from '../../../images/Typescript_logo.svg.png'
 
 export const TechStrip = () => {
-
-
     return (
-    <div>
-        <p>tech strip here</p>
-        <img src={GithubLogo.src} alt="Github Logo" />
+    <div className="display flex flex-direction column justify-around">
+        <a href='https://github.com/AdamFlatley' target="_blank" rel="noopener noreferrer">
+            <img src={GithubLogo.src} alt="Github Logo" />
+        </a>
         <img src={GQLLogo.src} alt="GraphQL Logo" />
+        <img src={JavaLogo.src} alt="Java Logo" />   
+        <img src={SpringBootLogo.src} alt="Spring Boot Logo" />
+        <img src={TSLogo.src} alt="TypeScript Logo" />
     </div>
+
 )
 }
