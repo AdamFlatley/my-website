8da50415d48c72e2f15d47c3216af378203caef2
Adam Flatley <flatleyadam@yahoo.co.uk>
2023-10-10 23:10:20 +0100

    slight style update for header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,26 +3,6 @@ import React from 'react'
 import Headshot from '../../../images/Headshot.png'
 import HeaderTab from '../sub-components/header-tab/header-tab';
 
-
-
-// const Header = () => {
-      
-// return (
-//     <div className="flex m-10 justify-around">
-//         <div className="text-center">
-//             <img
-//             src={Headshot.src}
-//             className="w-32 rounded-full shadow-lg"
-//             alt="Avatar" />
-//             <h5 className="mb-2 text-xl font-medium leading-tight">Adam Flatley</h5>
-//             <p className="text-neutral-500 dark:text-neutral-400">Full Stack Developer</p>
-//         </div>
-//         <HeaderTab TabName={'Home'} TabIcon={'NA'} />
-//         <HeaderTab TabName={'About'} TabIcon={'NA'} />
-//         <HeaderTab TabName={'Blog'} TabIcon={'NA'} />
-//     </div>
-// )
-// }
 const Header = () => {
     return (
         <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
@@ -31,16 +11,13 @@ const Header = () => {
                 src={Headshot.src}
                 className="w-16 rounded-full shadow-lg m-3"
                 alt="Avatar" />
-                <span className="font-semibold text-xl tracking-tight m-3">Adam Flatley</span>
-                <p className="text-white dark:text-neutral-400 m-3">Full Stack Developer</p>
+                <div className='flex-col'>
+                    <span className="font-semibold text-xl tracking-tight ml-2 mr-2 mb-1">Adam Flatley</span>
+                    <p className="text-white dark:text-neutral-400 ml-2 mr-2 mt-1">Full Stack Developer</p>
+                </div>
             </div>
-            <div className="block lg:hidden">
-                <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-                </button>
-            </div>
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                <div className="text-sm lg:flex-grow">
+            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto ml-2">
+                <div className="text-med lg:flex-grow">
                     <HeaderTab TabName={"Blog"} />
                     <HeaderTab TabName={"About"} />
                     <HeaderTab TabName={"Links"} />
--- src/components/sub-components/header-tab/header-tab.tsx
+++ src/components/sub-components/header-tab/header-tab.tsx
@@ -4,7 +4,7 @@ import { HeaderTypeProps } from './header-tab.types';
 
 const HeaderTab = (props: HeaderTypeProps) => {
   return (
-        <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 hover:animate-grow">
+        <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white m-4 hover:animate-grow">
           {props.TabName}
         </a>
   )
