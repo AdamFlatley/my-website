e84dfc7b5b48c1e0117a6240a8610ec951091d78
Adam Flatley <flatleyadam@yahoo.co.uk>
2023-11-16 00:21:33 +0000

    add new homepage boxes/tickets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,17 @@ import Header from '@/components/header/header';
 import { Modal, Button, Form } from "react-bootstrap";
 import SuitImage from '../../images/399858265_6925222990850265_5672313231962012675_n.jpg'
 import { HomePageBox } from '@/components/sub-components/home-page-box/home-page-box';
+import HonestyImage from '../../images/honesty.jpg'
+import CollaborationImage from '../../images/collaboration.jpg'
+import LearningImage from '../../images/learning.jpg'
+
+
 
 export default function HomePage() {
   const [showModal, setShowModal] = useState(true)
 
   return (
-    <main className='flex flex-col max-w-screen-xl mx-auto'>
+    <main className='flex flex-col max-w-screen-xl mx-auto divide-y'>
       <div className='flex flex-row max-w-screen-2xl'>
           <div  className='flex flex-col  mx-auto mb-auto mt-32 place-content-evenly'>
             <h1 className='text-4xl mb-4'>Meet Adam</h1>
@@ -23,15 +28,18 @@ export default function HomePage() {
           <img className='max-w-sm rounded-full shadow-2xl m-3 justify-self-end hover:animate-wiggle'
                     alt='Avatar' src={SuitImage.src} />
       </div>
-      <div className='flex flex-row space-between mx-auto max-w-screen-xl'>
-        <div className='my-0 -mx-5'>
-        <HomePageBox header={'mebox'} description={'medescription'} color='bg-tertiary-color'/>
-        </div>
-        <div className='my-10 -mx-5'>
-        <HomePageBox header={'mebox'} description={'medescription'} color='bg-secondary-color'/>
-        </div>
-        <div className='my-20 -mx-5'>
-        <HomePageBox header={'mebox'} description={'medescription'} color='bg-fourth-color'/>
+      <div className='flex flex-col mt-6 text-4xl font-semibold'>
+        <h2 className='divide-y justify-self-center align-center my-6 mx-auto'>My Values</h2>
+        <div className='flex flex-row space-between mx-auto max-w-screen-xl'>
+          <div className='my-0 -mx-5'>
+          <HomePageBox header={'Collaboration'} imageSrc={CollaborationImage.src} description={'I enjoy teaching what I know to others. I\'ll try to do this by mentoring or even by my blog posts (check them out!)'} color='bg-tertiary-color'/>
+          </div>
+          <div className='my-10 -mx-5'>
+          <HomePageBox header={'Honesty'} imageSrc={HonestyImage.src} description={'I consider myself an honest person - and I feel it is a quality that has placed me in a fortuitous position throughout my career. I appreciate those around me being honest too, it really helps everyone get the best out of each other.'} color='bg-secondary-color'/>
+          </div>
+          <div className='my-20 -mx-5'>
+          <HomePageBox header={'Learning'}  imageSrc={LearningImage.src} description={'If there\'s anything I feel is true about software development, it\'s that you\'re always learning. If this wasn\'t something I deemed important and found immense enjoyment in, I\'d be in the wrong line of work.'} color='bg-fourth-color'/>
+          </div>
         </div>
       </div>
     </main>
--- src/components/sub-components/home-page-box/home-page-box.tsx
+++ src/components/sub-components/home-page-box/home-page-box.tsx
@@ -4,15 +4,19 @@ export type HomePageBoxProps = {
     header: string,
     description: string,
     color: string,
+    imageSrc:string
 }
 
 export const HomePageBox = (props: HomePageBoxProps) => {
-    let mainClassTailwind =  ' flex flex-col items-center rounded-sm shadow-2xl h-96 w-96 hover:animate-grow hover:z-50 ' + props.color
+    let mainClassTailwind =  ' flex flex-col items-center rounded-sm shadow-2xl h-96 w-96 hover:animate-grow hover:z-50 border-8 ' + props.color
 
     return (
     <div className={mainClassTailwind} >
-        <h2 className='font-Consolas text-xl font-semibold'>{props.header}</h2>
-        <b className='text-lg'>{props.description}</b>
+        <h2 className='font-Consolas text-2xl font-semibold m-4'>{props.header}</h2>
+        <img
+            className=''
+            src={props.imageSrc} />
+        <b className=' text-lg font-normal m-8 invisible hover:visible'>{props.description}</b>
     </div>
 )
 }
