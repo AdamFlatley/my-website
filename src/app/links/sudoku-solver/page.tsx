c7da5f0bf6a48162d2e601c20141b2180d1a194a
Adam Flatley <flatleyadam@yahoo.co.uk>
2023-11-20 00:07:10 +0000

    work on init empty board

--- src/app/links/sudoku-solver/page.tsx
+++ src/app/links/sudoku-solver/page.tsx
@@ -5,25 +5,65 @@ import dynamic from "next/dynamic";
 import Footer from '@/components/footer/footer'
 import MainBody from '@/components/about-body.tsx/about-body';
 
+type Square = {
+  // aim here will be for key to be row + col, e.g '11' for index 1 for both
+  
+  key: string,
+  row: string,
+  col: string,
+  content: string
+}
+
+type Row = {
+  // aim here will be for key to be row + col, e.g '11' for index 1 for both
+  key: string,
+  content: Square[]
+}
+
 export default function LinksPage() {
+  const initEmptyBoard = () => {
     const length = 9
     const height = 9
-    let sudokuBoard = [...Array(9)].map(e => Array(9).map());
+    let sudokuBoard = []
+    let row = []
+    for (let i = 0; i < 9; i++) {
+      //could be an issue with overwriting reference
+      let row: Square[] = []
+      for (let j = 0; j < 9; j++) {
+        row.push({
+          key: '' + i + j,
+          row: '' + i,
+          col: '' + j,
+          content: ''
+        })
+      }
+      sudokuBoard.push(
+        {
+          key: i,
+          row: row
+        }
+        )
+    }
+    return sudokuBoard
+  }
+
+  const board = initEmptyBoard()
 
   return (
     <main className='flex flex-col max-w-screen-xl mx-auto divide-y'>
         <h1 className='text-4xl mb-4'>Sudoku Solver</h1> 
         <h2 className='text-2xl'>Enter your Sudoku Puzzle and this program will provide a solution - Given there is one! </h2>
-        <div className='mx-auto'>
-            {sudokuBoard.map((row) => {
-              return (<div>
-                {row.map(() => {
-                  return <input type="text" className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300  focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder='1'/>})
+        <div className='mx-auto max-h-xl'>
+            {board.map((row) => {
+              return (<div className='flex flex-row max-w-xl'>
+                {row.row.map((square) => {
+                  return <input type="text" className="block w-5 border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300  focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder=' '/>})
                 }
                 </div>)
             })}
         </div>
-
+        <button>Solve</button>
+        <button>Reset</button>
     </main>
   )
 }
