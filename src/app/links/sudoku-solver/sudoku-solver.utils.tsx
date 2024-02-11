b523690bef85b28d288549dd68e5c0f1ec3182e2
Adam Flatley <flatleyadam@yahoo.co.uk>
2024-02-11 17:05:50 +0000

    add in sudoku solving code

--- src/app/links/sudoku-solver/sudoku-solver.utils.tsx
+++ src/app/links/sudoku-solver/sudoku-solver.utils.tsx
@@ -16,4 +16,47 @@ export const hasMinimumSquares = (board: Row[]) => {
     })
     return false
 
-  }
+}
+
+export const solveSudoku = (board: Row[]) => {
+    for(let i = 0; i < 9; i++){
+      console.log(i)
+      for(let j = 0; j < 9; j ++){
+        if (board[i].row[j].content === ''){
+          for(let k = 1; k < 10; k++) { 
+            if(isPossible(board, k, i, j))
+            board[i].row[j].content = '' + k
+            solveSudoku(board)
+            board[i].row[j].content = ''
+        }
+        return
+      }
+      return board
+    }
+    }
+    return board
+}
+  
+export const isPossible = (board: Row[], value: number, col: number, row: number) => {
+for(let i = 0; i < 9; i ++){
+    if (board[row].row[i].content === '' + value) {
+    return false
+    }
+}
+for(let i = 0; i < 9; i ++){
+    if (board[i].row[col].content === '' + value) {
+    return false
+    }
+}
+let x0 = (Math.floor(row/3))*3
+let y0 = (Math.floor(col/3))*3
+for(let i = 0; i < 3; i ++){
+    for(let j = 0; j < 3; j++) { 
+        if (board[y0+i].row[x0+j].content === '' + value) {
+            return false
+        }
+    }
+}
+console.log('reached')
+return true
+}
